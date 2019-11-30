ee48e07d12b0bea34f03a75b3af3ecd6736e538f
PatrickTumulty <pats3mail@gmail.com>
2019-11-30 00:18:04 -0600

    removed audio examples

--- loop.py
+++ loop.py
@@ -1,13 +1,2 @@
-import SignalLibrary as sl
-import matplotlib.pyplot as plt 
 
-audio_file = "pt_example2.wav"
 
-sig = sl.WavFile(audio_file)
-sig.data = sig.normalize(sig.data, 1)
-env = sig.get_env_pd(sig.data)
-
-r = sig.loopGravity(sig.data, env, 0.02, 0.001, 10, 2, 2, 20, 40, graph=True)
-# plt.stem(sig.panValues)
-# plt.show()
-# sig.write2wav("pt_example1v2_g4_4loops_r3.wav", r)
--- test.py
+++ test.py
@@ -1,7 +1,21 @@
 
 import AudGrav
 
-io = AudGrav.AudioIO("pt_example2.wav")
+audio_file = "Example.wav"
+
+io = AudGrav.AudioIO(audio_file)
 sig = AudGrav.AudioAnalysis(io.data, io.sample_rate)
+
 env = sig.get_env_peak(sig.data)
-r = sig.loop_gravity(sig.data, env, 0.1, 0.001, 10, 3, plot=True)
+
+gConst = 4          # gravitational constant
+number_of_loops = 4 # number of times you want to loop over the data
+attack = 0.1        # attack threshold
+release = 0.001     # release threshold
+panRatio = 5
+panThresh = 30
+plot = False        # plot each iteration
+
+r = sig.loop_gravity(sig.data, env, attack, release, number_of_loops, gConst, panRatio, panThresh, plot)
+
+io.writeWav("NewFile.wav", r)
