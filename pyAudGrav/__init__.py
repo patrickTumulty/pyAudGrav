aa08706d829cddaecccf7dcf46df1e948dd970da
PatrickTumulty <pats3mail@gmail.com>
2019-12-05 14:27:27 -0600

    Update __init__.py

--- pyAudGrav/__init__.py
+++ pyAudGrav/__init__.py
@@ -3,3 +3,4 @@ from pyAudGrav.AudioEvent import AudioEvent
 from pyAudGrav.AudioReconstruct import AudioReconstruct
 from pyAudGrav.AudioIO import AudioIO
 
+example = AudioIO("/AudioExamples/Peak3.wav")
