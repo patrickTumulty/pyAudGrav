9b913c10dae8afe70f99b67bda34bbead4db27ca
PatrickTumulty <pats3mail@gmail.com>
2019-12-05 14:59:13 -0600

    fix

--- pyAudGrav/Examples.py
+++ pyAudGrav/Examples.py
@@ -1,5 +1,8 @@
 import os
 from pyAudGrav.AudioIO import AudioIO
 
-audio_file_path = os.getcwd() + "/AudioExamples"
-example1 = AudioIO(audio_file_path + "Peak3.wav")
+
+def readEx1():
+    audio_file_path = os.getcwd() + "/AudioExamples"
+    return AudioIO(audio_file_path + "Peak3.wav")
+
--- pyAudGrav/__init__.py
+++ pyAudGrav/__init__.py
@@ -2,4 +2,4 @@ from pyAudGrav.AudioAnalysis import AudioAnalysis
 from pyAudGrav.AudioEvent import AudioEvent
 from pyAudGrav.AudioReconstruct import AudioReconstruct
 from pyAudGrav.AudioIO import AudioIO
-import pyAudGrav.Examples
+from pyAudGrav.Examples import readEx1
