3adc3340e52e729784bc906145b138011f7ff030
PatrickTumulty <pats3mail@gmail.com>
2019-12-05 15:06:12 -0600

    test

--- pyAudGrav/Examples.py
+++ pyAudGrav/Examples.py
@@ -1,8 +1,9 @@
-import os
+
 from pyAudGrav.AudioIO import AudioIO
+from distutils.sysconfig import get_python_lib
 
 
 def readEx1():
-    audio_file_path = os.getcwd() + "/AudioExamples"
+    audio_file_path = get_python_lib +  "pyAudGrav/AudioExamples"
     return AudioIO(audio_file_path + "/Peak3.wav")
 
