bc338a74bac8284d36b42e1762a9cd797d4f3741
PatrickTumulty <pats3mail@gmail.com>
2019-12-05 14:48:42 -0600

    testing

---
+++ pyAudGrav/Examples.py
@@ -0,0 +1,5 @@
+import os
+from pyAudGrav.AudioIO import AudioIO
+
+audio_file_path = os.getcwd() + "/AudioExamples"
+example1 = AudioIO(audio_file_path + "Peak3.wav")
--- pyAudGrav/__init__.py
+++ pyAudGrav/__init__.py
@@ -3,4 +3,3 @@ from pyAudGrav.AudioEvent import AudioEvent
 from pyAudGrav.AudioReconstruct import AudioReconstruct
 from pyAudGrav.AudioIO import AudioIO
 
-example = AudioIO("/AudioExamples/Peak3.wav")
