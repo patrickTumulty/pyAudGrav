d4bc68a4022aa87e05ceab3080822810ee66b779
PatrickTumulty <pats3mail@gmail.com>
2019-12-05 23:27:38 -0600

    added audio examples

--- pyAudGrav/__init__.py
+++ pyAudGrav/__init__.py
@@ -2,4 +2,4 @@ from pyAudGrav.AudioAnalysis import AudioAnalysis
 from pyAudGrav.AudioEvent import AudioEvent
 from pyAudGrav.AudioReconstruct import AudioReconstruct
 from pyAudGrav.AudioIO import AudioIO
-from pyAudGrav.audio_files import load_example1, load_example2, load_example3, load_example4, load_example5 
+from pyAudGrav.Examples import load_example1, load_example2, load_example3, load_example4, load_example5 
